5c19fef67895b0b9356c29a90effb6e4bb376c08
ariqqqq <hariqhariq407@gmail.com>
2024-01-08 16:16:40 +0700

    menambah file

---
+++ operasi-aritmatika.py
@@ -0,0 +1,7 @@
+# operasi aritmatika sederhana
+var1 = 10 # tipe data integer krna bilangan bulat
+var2 = 10
+print("Hasil penambahannya = ", var1+var2)
+print("Hasil penguranganya = ", var1-var2)
+print("Hasil pembagiannya = ", var1/var2)
+print("Hasil perkaliannya = ", var1*var2)
